fae6fda0845cfe732b26bba413eea817a67314f2
chacha <86880916+hansejun@users.noreply.github.com>
2025-11-18 22:22:25 +0900

    feat: core/render 구현

--- packages/react/src/core/render.ts
+++ packages/react/src/core/render.ts
@@ -1,5 +1,5 @@
 import { context } from "./context";
-import { getDomNodes, insertInstance } from "./dom";
+import { insertInstance } from "./dom";
 import { reconcile } from "./reconciler";
 import { cleanupUnusedHooks } from "./hooks";
 import { withEnqueue } from "../utils";
@@ -10,9 +10,28 @@ import { withEnqueue } from "../utils";
  */
 export const render = (): void => {
   // 여기를 구현하세요.
+
   // 1. 훅 컨텍스트를 초기화합니다.
+  context.hooks.clear();
+
   // 2. reconcile 함수를 호출하여 루트 노드를 재조정합니다.
+  const { container, node, instance } = context.root;
+
+  if (!container || !node) {
+    throw new Error("컨테이너 또는 노드가 없습니다.");
+  }
+
+  const newInstance = reconcile(container, instance, node, "0");
+
+  // 첫 렌더링 시 루트 노드를 삽입
+  if (newInstance && !instance) {
+    insertInstance(container, newInstance);
+  }
+
+  context.root.instance = newInstance;
+
   // 3. 사용되지 않은 훅들을 정리(cleanupUnusedHooks)합니다.
+  cleanupUnusedHooks();
 };
 
 /**
